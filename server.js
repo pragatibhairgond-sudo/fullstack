aec7edd30b9a81b897100ea96925ec61c0017f29
pragatibhairgond-sudo <pragatibhairgond@gmail.com>
2026-07-22 22:06:56 +0530

    server.js

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
+// Set Up Express Application
 const express = require('express');
 const app = express();
 
+// home route
 app.get('/', (req, res) => {
     res.send('Hello World');})
 
+// Start Server
 app.listen(3000, () => { 
     console.log('Sever is running on https://localhost:3000');});
